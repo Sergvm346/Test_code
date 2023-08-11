8c07bedc0e55714bb5017254c495a3c1352bc39c
Sergey <serg-v-m@yandex.ru>
2023-08-11 19:20:44 +0400

    Test commit 1

---
+++ test.js
@@ -0,0 +1,29 @@
+// function testdata(data) {
+//   data.forEach(function (items) {
+//     console.log("values ", items);
+//     items.forEach(function (number) {
+//       console.log("Marks", number); //
+//     });
+//   });
+// }
+
+// const test = [
+//   ["maths", 52],
+//   ["science", 65],
+//   ["english", 72],
+// ];
+// testdata(test);
+
+
+
+function log(n) {
+  for (let i = 1; i <= n; i = i * 2) {
+    const result = i;
+    console.log(result);
+  }
+}
+log(4);
+console.log("11!!!")
+
+
+console.log("!!!")
